d85606928636602e72d835ee4be280adea76336c
Mahamudul-Nobi-Mohon <mn.mohon@gmail.com>
2016-10-14 01:15:08 +0600

    RDLC Report Asp.Net web form application

---
+++ Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+
+        </asp:ScriptManager>
+        <div>
+            <asp:Button ID="button1" Text="Show Report" runat="server" OnClick="button1_Click" />
+        </div>
+    <div>
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server" Width="80%"></rsweb:ReportViewer>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.Reporting.WebForms;
+
+public partial class _Default : System.Web.UI.Page
+{
+    String con = System.Configuration.ConfigurationManager.ConnectionStrings["PhoneNumberConnectionString"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void button1_Click(object sender, EventArgs e)
+    {
+        ReportViewer1.Reset();
+        ReportDataSource reptsrc = new ReportDataSource("DataSet1",GetData());
+        ReportViewer1.LocalReport.DataSources.Add(reptsrc);
+        ReportViewer1.LocalReport.ReportPath = "Report.rdlc";
+        ReportViewer1.LocalReport.Refresh();
+
+    }
+
+    private DataTable GetData()
+    {
+        DataTable dt = new DataTable();
+        using (SqlConnection cn = new SqlConnection(con))
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("select name,phonenumber,gender,description from phonenumber", cn);
+            adp.Fill(dt);
+        }
+        return dt;
+    }
+
+
+}
